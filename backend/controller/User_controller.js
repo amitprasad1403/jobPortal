82f888d4f7469a1ffd9ad6b2b81a96857a7b786a
amitprasad1403 <146555734+amitprasad1403@users.noreply.github.com>
2024-03-05 23:45:16 +0530

    User Controller validations

--- backend/controller/User_controller.js
+++ backend/controller/User_controller.js
@@ -1,7 +1,10 @@
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const {body,validationResult} = require('express-validator');
 
 const getAllUsers = async (req, res, next) => {
     try{
+        //Get Query
         const users = await User.find();
         return res.status(200).json({users});
     }
@@ -13,13 +16,26 @@ const getAllUsers = async (req, res, next) => {
 
 const addUser = async (req, res, next) => {
     try{
+        //Validation in express
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // Extract specific error messages
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.status(400).json({ errors: errorMessages });
+        }
+
+        //To incrypt the password
+        const salt = await bcrypt.genSalt(10);
+        const secure_pass = await bcrypt.hashSync(req.body.pass, salt);
+
+        //Add query
         const add_user = User.create({
             name:req.body.name,
             email:req.body.email,
             mobile_number:req.body.mobile_number,
             dob:req.body.dob,
             username:req.body.username,
-            pass:req.body.pass
+            pass:secure_pass
         })
         return res.status(200).json({success:true});
     }
--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const {getAllUsers,addUser} = require("../controller/User_controller")
+const express = require('express');
+const {body} = require('express-validator');
+const {getAllUsers,addUser} = require("../controller/User_controller");
 const router = express.Router();
 
 router.get('/',getAllUsers);
-router.post('/add',addUser);
+router.post('/add',[
+        body('email','it should be email').isEmail(),
+        body('name').isLength({min:5}),
+        body('pass','password must be least 5 lenth').isLength({min:5})
+    ],addUser);
 
 module.exports = router;
